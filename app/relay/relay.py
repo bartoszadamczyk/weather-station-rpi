bfdde32654dad7c1e8a1f2672160a895601550f4
Bartosz Adamczyk <3093076+bartoszadamczyk@users.noreply.github.com>
2021-02-11 20:14:44 +0000

    Fix relay states

--- app/relay/relay.py
+++ app/relay/relay.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO  # type: ignore
 
-GPIO.setwarnings(False)
+# GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
 
@@ -8,15 +8,15 @@ class Relay:
     def __init__(self, pin: int) -> None:
         print(f"Setup relay on pin {pin}")
         self.pin = pin
-        GPIO.setup(pin, GPIO.OUT)
+        GPIO.setup(pin, GPIO.OUT, initial=GPIO.HIGH)
 
     def up(self) -> None:
         print(f"Set pin {self.pin} up")
-        GPIO.output(self.pin, GPIO.HIGH)
+        GPIO.output(self.pin, GPIO.LOW)
 
     def down(self) -> None:
         print(f"Set pin {self.pin} down")
-        GPIO.output(self.pin, GPIO.LOW)
+        GPIO.output(self.pin, GPIO.HIGH)
 
 
 def cleanup_gpio() -> None:
