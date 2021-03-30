8ebf651ddb77a399b30162afa9e4c12fab42c41b
Bartosz Adamczyk <3093076+bartoszadamczyk@users.noreply.github.com>
2021-03-30 16:18:56 +0100

    Try to serialize str

--- app/aws.py
+++ app/aws.py
@@ -10,4 +10,4 @@ QUEUE_URL = os.environ["AWS_SQS_DATA"]
 
 
 def send_message_to_sqs(message: dict):
-    return sqs.send_message(QueueUrl=QUEUE_URL, MessageBody=json.dumps(message))
+    return sqs.send_message(QueueUrl=QUEUE_URL, MessageBody=json.dumps(message, default=str))
