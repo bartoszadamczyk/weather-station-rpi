777e7ff8a6fe50e03b0a3af586190d6aa88a3944
Bartosz Adamczyk <3093076+bartoszadamczyk@users.noreply.github.com>
2021-04-06 23:04:51 +0100

    Add CPU Sensor

--- app/sensor.py
+++ app/sensor.py
@@ -10,15 +10,15 @@ from .reading import ReadingCollection, Reading
 
 
 class MODEL:
+    CPU = "CPU"
     DHT22 = "DHT22"
     DS18B20 = "DS18B20"
 
 
 class Sensor(ABC):
-    def __init__(self, device_uuid: str, pointer: Union[DHT22, W1ThermSensor]):
+    def __init__(self, device_uuid: str):
         super().__init__()
         self.device_uuid = device_uuid
-        self.pointer = pointer
         self.reading_collection = ReadingCollection()
 
     @property
@@ -37,10 +37,9 @@ class Sensor(ABC):
 
 
 class DHT22Sensor(Sensor):
-    def __init__(
-        self, device_uuid: str, pointer: Union[DHT22, W1ThermSensor], pin: Pin
-    ):
-        super().__init__(device_uuid, pointer)
+    def __init__(self, device_uuid: str, pointer: DHT22, pin: Pin):
+        super().__init__(device_uuid)
+        self.pointer = pointer
         self.pin = pin
 
     @property
@@ -75,6 +74,10 @@ def create_dht22_sensor(device_uuid: str, pin: int) -> DHT22Sensor:
 
 
 class DS18B20Sensor(Sensor):
+    def __init__(self, device_uuid: str, pointer: W1ThermSensor):
+        super().__init__(device_uuid)
+        self.pointer = pointer
+
     @property
     def id(self) -> str:
         return self.pointer.id
@@ -101,14 +104,44 @@ def discover_ds18b20_sensors(
     ]
 
 
+class CPUSensor(Sensor):
+    @property
+    def id(self) -> str:
+        return "cpu"
+
+    @property
+    def model(self) -> str:
+        return MODEL.CPU
+
+    def _get_cpu_temperature(self) -> float:
+        with open("/sys/class/thermal/thermal_zone0/temp", "r") as cpu_temperature_file:
+            return int(cpu_temperature_file.read()) / 1000
+
+    def get_reading(self, delay: int = 0) -> Reading:
+        time.sleep(delay)
+        reading = Reading(
+            self.device_uuid, self.id, self.model, self._get_cpu_temperature()
+        )
+        self.reading_collection.add_reading(reading)
+        return reading
+
+
+def create_cpu_sensor(
+    device_uuid: str,
+) -> CPUSensor:
+    return CPUSensor(device_uuid)
+
+
 class SensorCollection:
     def __init__(self, device_uuid: str, pins: Optional[List[int]] = None):
+        self.cpu_collection = [create_cpu_sensor(device_uuid)]
         self.ds18b20_collection = discover_ds18b20_sensors(device_uuid)
         self.dht22_collection = (
             [create_dht22_sensor(device_uuid, pin) for pin in pins] if pins else []
         )
         self._lookup = {
-            **{sensor.id: sensor for sensor in self.ds18b20_collection},
+            **{sensor.id: sensor for sensor in self.cpu_collection},
+            **{sensor.id: sensor for sensor in self.ds18b20_collection},  # type: ignore
             **{sensor.id: sensor for sensor in self.dht22_collection},  # type: ignore
         }
 
@@ -119,5 +152,6 @@ class SensorCollection:
         return self._lookup[sensor_id]
 
     def __iter__(self):
+        yield from self.cpu_collection
         yield from self.ds18b20_collection
         yield from self.dht22_collection
